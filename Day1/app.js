ab1ba81b32025e8ce42b35ddaad8e1996435539e
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-06-08 17:33:54 +0500

    Day 01
    Interval function and build-In keywords

--- Day1/app.js
+++ Day1/app.js
@@ -1,9 +1,15 @@
 const amount = 30;
 
 if (amount < 50) {
-    console.log('Small Number')
+  console.log("Small Number");
 } else {
-    console.log('Large Number')
+  console.log("Large Number");
 }
 
-console.log('This is my first App')
+console.log("This is my first App");
+
+setInterval(() => {
+  console.log("This is Interval function");
+}, 1000);
+
+console.log(__filename)
