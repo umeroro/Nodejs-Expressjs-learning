d36e73b6aa8b35bfde6ddad358cee94c14c9786b
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-06-09 14:46:21 +0500

    Day 01
    Direct import the file

--- Day1/3-modules.js
+++ Day1/3-modules.js
@@ -1,5 +1,6 @@
 const { duration, position } = require("./modules");
 const { info } = require("./2-modules");
+require("./grenade");
 
 info(position);
 info(duration);
---
+++ Day1/grenade.js
@@ -0,0 +1,8 @@
+const a = 12;
+const b = 34;
+
+function sum() {
+  console.log(a + b);
+}
+
+sum();
